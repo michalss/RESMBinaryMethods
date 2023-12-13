70ac13dd1f875beaa9b4401854e3526d49fd1f2a
michal.seckar <michal.seckar@notino.com>
2023-12-13 21:33:38 +0100

    overloaded func with string instead of enums

--- RESM.AlgosComps/Algos.cs
+++ RESM.AlgosComps/Algos.cs
@@ -26,14 +26,39 @@ namespace RESM.AlgosComps
             set { _buffer = value; }
         }
 
-        public enum DeCompType
+        public enum  DeCompType 
         {
             zlib,
-            lzo1x, 
+            lzo1x,
             lz4,
+            lz4x,
             deflate,
             lzo1a,
-            lzss
+            lzss,
+            gzip,
+            bzip2,
+            lzma,
+            lz4_stream,
+            oodle,
+            oodle_lzh,
+            oodle_lzhlw,
+            oodle_lznib,
+            oodle_lzb16,
+            oodle_lzblw,
+            oodle_lzna,
+            oodle_bitknit,
+            oodle_lza,
+            oodle_lzq1, oodle_kraken,
+            oodle_lznib2, oodle_mermaid,
+            segs,
+            oodle_selkie,
+            oodle_akkorokamui,
+            lzo2a,
+            zlib_noerror,
+            deflate_noerror,
+            base64,
+            xmemdecompress,
+            ascii85
 
         }
         public enum CompType
@@ -41,9 +66,17 @@ namespace RESM.AlgosComps
             zlib_compress,
             lzo1x_compress,
             lz4_compress,
+            lz4x_compress,
+            lz4x_new_compress,
             deflate_compress,
             lzo1a_compress,
-            lzss_compress
+            lzss_compress,
+            gzip_compress,
+            bzip2_compress,
+            lzma_compress,
+            lzo2a_compress,
+            oodle_compress,
+            xmemlzx_compress
         }
 
         public enum EcryptType
@@ -75,8 +108,64 @@ namespace RESM.AlgosComps
             return decompressedData;
         }
 
+        public byte[] Decompress(string algo, byte[] compressData, int zsize, int size = 0)
+        {
+            if (size == 0)
+            {
+                size = _buffer; // use 1 MB if not sure
+            }
+
+            byte[] outData = new byte[size];
+            var return_data = quickbms_compression(algo.ToString(), compressData, zsize, outData, size);
+
+            if (return_data < 0)
+            {
+                return_data = 0;// if 0 means some error not sure need to handle it!
+            }
+
+            var decompressedData = new byte[return_data];
+            if (size > 0)
+            {
+                Array.Copy(outData, decompressedData, return_data);
+            }
+
+            return decompressedData;
+        }
+
 
         public byte[] Decompress(DeCompType algo, byte[] dictionary, byte[] compressedData, int size = 0)
+        {
+
+            byte[] decompressedData;
+
+            if (compressedData.Length == 0)
+            {
+                decompressedData = new byte[0];
+                return [];
+            }
+
+            if (size == 0)
+                size = _buffer; // Use 1 MB if not sure
+
+            byte[] outData = new byte[size];
+
+            int return_size = quickbms_compression2(algo.ToString(), dictionary, dictionary.Length, compressedData, compressedData.Length, outData, size);
+
+            if (return_size < 0)
+                return_size = 0; // Handle error codes simply - creates empty array in case of error
+
+            decompressedData = new byte[return_size];
+
+            // Copy data from native buffer to managed buffer
+            if (return_size > 0)
+            {
+                Array.Copy(outData, decompressedData, return_size);
+            }
+
+            return decompressedData;
+        }
+
+        public byte[] Decompress(string algo, byte[] dictionary, byte[] compressedData, int size = 0)
         {
 
             byte[] decompressedData;
@@ -137,8 +226,37 @@ namespace RESM.AlgosComps
             }
 
             return compressedData;
+        }
 
+        public byte[] Compress(string algo, byte[] decompressedData, int size = 0)
+        {
+            byte[] compressedData;
 
+            if (decompressedData.Length == 0)
+            {
+                compressedData = new byte[0];
+                return [];
+            }
+
+            if (size == 0)
+                size = _buffer; // Use 1 MB if not sure
+
+            byte[] outData = new byte[size];
+
+            int return_size = quickbms_compression(algo.ToString(), decompressedData, decompressedData.Length, outData, size);
+
+            if (return_size < 0)
+                return_size = 0; // Handle error codes simply - creates empty array in case of error
+
+            compressedData = new byte[return_size];
+
+            // Copy data from native buffer to managed buffer
+            if (return_size > 0)
+            {
+                Array.Copy(outData, compressedData, return_size);
+            }
+
+            return compressedData;
         }
 
         public byte[] Compress(CompType algo, byte[] dictionary, byte[] decompressedData, int size = 0)
@@ -169,6 +287,33 @@ namespace RESM.AlgosComps
             return compressedData;
         }
 
+        public byte[] Compress(string algo, byte[] dictionary, byte[] decompressedData, int size = 0)
+        {
+            byte[] compressedData;
+            if (decompressedData.Length == 0)
+            {
+                compressedData = new byte[0];
+                return [];
+            }
+
+            if (size == 0)
+                size = _buffer; // Use 1 MB if not sure
+
+            byte[] outData = new byte[size];
+
+            int return_size = quickbms_compression2(algo.ToString(), dictionary, dictionary.Length, decompressedData, decompressedData.Length, outData, size);
+
+            if (return_size < 0)
+                return_size = 0; // Handle error codes simply - creates empty array in case of error
+
+            compressedData = new byte[return_size];
+            if (return_size > 0)
+            {
+                Array.Copy(outData, compressedData, return_size);
+            }
+
+            return compressedData;
+        }
 
         public byte[] Encryption(EcryptType type, ref byte[] key, ref byte[] ivec, ref byte[] inputData)
         {
@@ -195,8 +340,58 @@ namespace RESM.AlgosComps
             return outputData;
         }
 
+        public byte[] Encryption(string type, ref byte[] key, ref byte[] ivec, ref byte[] inputData)
+        {
+            byte[] outputData;
+            if (inputData.Length == 0)
+            {
+                outputData = new byte[0];
+                return [];
+            }
+
+            byte[] inoutData = new byte[inputData.Length];
+            Array.Copy(inputData, inoutData, inputData.Length);
+
+            int size = quickbms_encryption(type.ToString(), key, key.Length, ivec, ivec.Length, 1, inoutData, inputData.Length);
+
+            if (size < 0) size = 0; // Handle error codes
+
+            outputData = new byte[size];
+            if (size > 0)
+            {
+                Array.Copy(inoutData, outputData, size);
+            }
+
+            return outputData;
+        }
+
 
         public byte[] Decryption(EcryptType type, ref byte[] key, ref byte[] ivec, ref byte[] inputData)
+        {
+            byte[] outputData;
+            if (inputData.Length == 0)
+            {
+                outputData = new byte[0];
+                return [];
+            }
+
+            byte[] inoutData = new byte[inputData.Length];
+            Array.Copy(inputData, inoutData, inputData.Length);
+
+            int size = quickbms_encryption(type.ToString(), key, key.Length, ivec, ivec.Length, 0, inoutData, inputData.Length);
+
+            if (size < 0) size = 0; // Handle error codes
+
+            outputData = new byte[size];
+            if (size > 0)
+            {
+                Array.Copy(inoutData, outputData, size);
+            }
+
+            return outputData;
+        }
+
+        public byte[] Decryption(string type, ref byte[] key, ref byte[] ivec, ref byte[] inputData)
         {
             byte[] outputData;
             if (inputData.Length == 0)
